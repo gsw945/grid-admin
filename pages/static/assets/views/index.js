fbe1d5fceb14e2a26f05ac35f26eeee59a4a4ed8
玖亖伍 <gsw945@foxmail.com>
2019-06-06 10:21:26 +0800

    remove 'Marionette.View.setRenderer'

--- pages/static/assets/views/index.js
+++ pages/static/assets/views/index.js
@@ -1,37 +1,34 @@
 define([
     'require', 'lodash', 'jquery', 'backbone', 'backbone.radio', 'backbone.marionette', 'handlebars'
 ], function(require, _, $, Backbone, Radio, Marionette, Handlebars) {
-    // 设置模板渲染
-    Marionette.View.setRenderer(function(template, data) {
-        return Handlebars.compile(template)(data);
-    });
     // 视图继承
     var View = Marionette.View.extend({
         tagName: 'div',
-        templateContext: function() {
-            var degree = this.getOption('degree');
-            return {
-                isDr: function() {
-                    return (degree === 'phd');
-                },
-                fullName: function() {
-                    return this.isDr() ? this.name : '无名氏';
-                }
-            };
+        templateContext: {
+            isDr: function() {
+                debugger;
+                return (this.degree) === 'phd';
+            },
+            fullName: function() {
+                debugger;
+                return this.isDr() ? "无名氏" : this.name;
+            }
         },
         defaults: {
             name: 'python'
         },
+        render: function() {
+            var self = this;
+            require(['text!assets/templates/index/demo.html'], function (template) {
+                debugger;
+                // debugger;
+                self.template = Handlebars.compile(template)
+                self.$el.html(self.template(self.model.attributes));
+            });
+        },
         className: 'bg-success',
         checkOption: function() {
             console.log(this.getOption('foo'));
-        },
-        prepareRender: function() {
-            var self = this;
-            require(['text!assets/templates/index/demo.html'], function (template) {
-                self.template = template;
-                self.render();
-            });
         },
         initialize: function() {
             // foo --> onFoo
@@ -48,13 +45,13 @@ define([
     // 视图
     var view = new View({
         foo: 'some text',
-        degree: 'master',
+        degree: 'phd',
         model: model
     });
     // 视图方法调用
     view.checkOption();
     // 渲染视图
-    view.prepareRender();
+    view.render();
     // 将渲染后的内容添加到页面上
     $('#article-region').empty().append(view.$el);
 });
